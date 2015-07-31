4e11c98f17a593602c46396ca450656b3da025fb
Dima Buzenkov <Tpyn666@gmail.com>
2015-07-31 19:32:21 +0300

    Мелочи

--- js/urepost-vk.js
+++ js/urepost-vk.js
@@ -33,25 +33,6 @@
 			var $elPost = $(this);
 			$('.urepost-vk-btn', $elPost).remove();
 		});
-	/*
-	 var posts = document.querySelectorAll('div .post.all.own');
-	
-	 for (var i = posts.length; i-- ;) {
-	 var post = posts[i];
-	
-	 console.dir(post.id);
-	
-	 post.addEventListener('click', function () {
-	 var id = this.id;
-	 chrome.runtime.sendMessage({id: id}, function(response) {
-	 console.log(response);
-	 });
-	 });
-	
-	 }
-	 //chrome.extension.getURL('');
-	 */
-	
 })();
 
 
