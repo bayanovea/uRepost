5dc235caefae6d698ae99d6c809bbc0027727673
Dima Buzenkov <Tpyn666@gmail.com>
2015-07-31 18:38:18 +0300

    Мелочи

--- js/background.js
+++ js/background.js
@@ -1,10 +1,18 @@
 console.log('start background');
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function (req, sender, cb) {
 	//console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
-	var postId = request.id.replace('post', '');
-	getVK.getPostById(postId, function (data) {
-		sendResponse({data: data});
-	});
+	
+	switch (req.method) {
+		case "vk.repost":
+			var postId = req.postId.replace('post', '');
+			getVK.getPostById(postId, function (data) {
+				cb({data: data});
+			});
+			break;
+		default :
+			cb({data: data});
+			break;
+	}
 	return true;
 });
