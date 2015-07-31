b260cdd424520a4ab1bc505d256fce5d33b04405
Dima Buzenkov <Tpyn666@gmail.com>
2015-07-31 12:26:54 +0300

    Иньекция в ВК

---
+++ js/injection-vk.js
@@ -0,0 +1,11 @@
+"use strict";
+
+console.log("VK [search posts]");
+
+var posts = document.querySelectorAll('div .post.all.own');
+
+for (var i = 0; i < posts.length; i ++) {
+	
+	console.dir(posts[i]);
+	
+}
